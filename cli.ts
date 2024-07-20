215a0eb01c8db488c5283ae602fb13e46fb912e8
Levi Bostian <levi@levi.earth>
2024-07-20 09:28:51 -0500

    get name of current branch

--- cli.ts
+++ cli.ts
@@ -1,4 +1,7 @@
 import { getLastGitHubRelease } from "./github.ts";
+import * as git from "./git.ts";
+
+console.log(`current git branch ${await git.getCurrentBranchName()}`)
 
 console.log(await getLastGitHubRelease("levibostian", "wendy-ios", "latest"))
 
