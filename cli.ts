7ed98fd6f00e40159f3b3ff1d40e7d0c8f355d05
Levi Bostian <levi@levi.earth>
2024-07-20 09:33:39 -0500

    get all required info needed to run code from env vars on github actions

--- cli.ts
+++ cli.ts
@@ -1,7 +1,12 @@
 import { getLastGitHubRelease } from "./github.ts";
 import * as git from "./git.ts";
 
-console.log(`current git branch ${await git.getCurrentBranchName()}`)
+const currentBranch = await git.getCurrentBranchName();
 
-console.log(await getLastGitHubRelease("levibostian", "wendy-ios", "latest"))
+console.log(`current git branch ${currentBranch}`)
+
+// example value for GITHUB_REPOSITORY: "denoland/deno"
+const [owner, repo] = Deno.env.get("GITHUB_REPOSITORY")!.split("/"); 
+
+console.log(await getLastGitHubRelease(owner, repo, currentBranch))
 
