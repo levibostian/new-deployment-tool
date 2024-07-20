00289f7aebc9e17084e946482332a0a2febdc34e
Levi Bostian <levi@levi.earth>
2024-07-20 15:28:39 -0500

    create new github release with new release version
    Note: I have not tested this code! I think that will be easiest to test when we run the code in a real github repo.

--- lib/github.ts
+++ lib/github.ts
@@ -1,3 +1,4 @@
+import { GitCommit } from "./type.ts";
 
 
 export const getLatestTagForBranch = async ({ owner, repo, branch }: { owner: string; repo: string; branch: string }) => {
@@ -41,6 +42,7 @@ export const getAllCommitsSinceGivenCommit = async({ owner, repo, branch, lastTa
   let commits: {
     sha: string,
     message: string, 
+    createdAt: Date
   }[] = [];  
   let hasNextPage = true;
   let url = `https://api.github.com/repos/${owner}/${repo}/commits?sha=${branch}&since=${lastTagSha}`;
@@ -50,15 +52,19 @@ export const getAllCommitsSinceGivenCommit = async({ owner, repo, branch, lastTa
       sha: string,
       commit: {
         message: string
+        author: {
+          date: string
+        }
       }      
     }[]>(url);    
 
     for (const commit of response.responseJsonBody) {
       commits = commits.concat({
         sha: commit.sha,
-        message: commit.commit.message
+        message: commit.commit.message,  
+        createdAt: new Date(commit.commit.author.date)
       });
-    }    
+    }
 
     if (!response.nextPageUrl) {
       hasNextPage = false;
@@ -67,16 +73,33 @@ export const getAllCommitsSinceGivenCommit = async({ owner, repo, branch, lastTa
     }
   }
 
+  // sort commits by date. first commit is the newest one
+  commits = commits.sort((a, b) => {
+    return a.createdAt.getTime() - b.createdAt.getTime();    
+  });
+
   return commits;
 }
 
-const githubApiRequest = async<T>(url: string): Promise<{ responseJsonBody: T, nextPageUrl: string | undefined }> => {
+export const createGitHubRelease = async ({ owner, repo, tagName, commit }: { owner: string; repo: string; tagName: string; commit: GitCommit }) => {
+  await githubApiRequest(`https://api.github.com/repos/${owner}/${repo}/releases`, 'POST', {
+    tag_name: tagName,
+    target_commitish: commit.sha,
+    name: tagName,
+    body: '',
+    draft: false,
+    prerelease: false
+  });
+}
+
+const githubApiRequest = async<T>(url: string, method: 'GET' | 'POST' = 'GET', body: object | undefined = undefined): Promise<{ responseJsonBody: T, nextPageUrl: string | undefined }> => {
   const headers = {
     'Authorization': `Bearer ${Deno.env.get("GITHUB_TOKEN")}`,
     'Accept': 'application/vnd.github.v3+json',
+    'Content-Type': 'application/json',
   };
 
-  const response = await fetch(url, { headers })
+  const response = await fetch(url, { method, headers, body: body ? JSON.stringify(body) : undefined });
 
   if (!response.ok) {
     throw new Error(`Failed to call github API endpoint: ${url}, given error: ${response.statusText}`);
